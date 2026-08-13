fa50e5464cdb2a924d8717197528cb4328055e5f
Bhuva Raj <rajbhuva91@gmail.com>
2026-08-13 10:46:27 +0530

    Time: 0 ms (100%), Space: 42.9 MB (69.14%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.java
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.java
@@ -1,42 +1,35 @@
 class Solution {
-    public int splitArray(int[] arr, int k) {
-        if (arr.length < k) {
-            return -1;
-        }
-        
-        long ans =-1;
-        long low=0;
-        long high=0;
-        for(int num : arr){
-          low=Math.max(low, num);
+    public int splitArray(int[] nums, int k) {
+        if(nums.length<k) return -1;
+
+        int low=0;
+        int high=0;
+        for(int num: nums){
+            low=Math.max(low, num);
             high+=num;
         }
-        while(low<=high){
-            long mid=low+(high-low)/2;
-            if(isvalid(arr, mid, k)){
-               ans = mid;
-                high=mid-1;
-                
+        while(low<high){
+            int mid=low+(high-low)/2;
+            if(isvalid(nums, k, mid)){
+                high=mid;
             }else{
                 low=mid+1;
             }
         }
-        return (int)ans;
-    }
-    private boolean isvalid(int[] arr,long mid,int k){
-        int count=1;
-        int cpages=0;
-        for(int pages : arr){
-            if(cpages+pages>mid){
-                cpages=pages;
-                count++;
-                
-            }
-            else{
-                cpages+=pages;
-            }
-        }
-        return count<=k;
+        return low;
     }
 
+    private boolean isvalid(int[] nums, int k, int mid){
+        int count=1;
+        int cs=0;
+       for(int num:nums){
+        if(cs+num>mid){
+            count++;
+            cs=num;
+        }else{
+            cs+=num;
+        }
+       }
+       return count<=k;
+    }
 }
